cec2001e5892875989c4d51ded61a63bc8f323dd
210-reverof <sheltonwon@gmail.com>
2023-06-24 13:48:18 +0900

    init: add project

---
+++ src/main/java/teo/sprint/navogue/NavogueApplication.java
@@ -0,0 +1,13 @@
+package teo.sprint.navogue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NavogueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NavogueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/teo/sprint/navogue/NavogueApplicationTests.java
@@ -0,0 +1,13 @@
+package teo.sprint.navogue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NavogueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
