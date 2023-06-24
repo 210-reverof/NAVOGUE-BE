1cb8cd23b0debd75ec21880468538788bb820df2
210-reverof <sheltonwon@gmail.com>
2023-06-24 23:53:21 +0900

    fix: 태그 레포 수정

---
+++ src/main/java/teo/sprint/navogue/domain/tag/repository/TagRepository.java
@@ -0,0 +1,7 @@
+package teo.sprint.navogue.domain.tag.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import teo.sprint.navogue.domain.tag.data.entity.Tag;
+
+public interface TagRepository extends JpaRepository<Tag, Integer> {
+}
