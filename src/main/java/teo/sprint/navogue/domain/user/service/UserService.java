0d43509117fe412c809e669d2c66dec31f2bf56a
210-reverof <sheltonwon@gmail.com>
2023-06-25 12:04:10 +0900

    feat: 로그인 완료

--- src/main/java/teo/sprint/navogue/domain/user/service/UserService.java
+++ src/main/java/teo/sprint/navogue/domain/user/service/UserService.java
@@ -5,6 +5,7 @@ import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import net.minidev.json.JSONObject;
 import org.springframework.stereotype.Service;
 import teo.sprint.navogue.common.security.jwt.JwtTokenProvider;
 import teo.sprint.navogue.domain.user.data.entity.User;
@@ -13,6 +14,7 @@ import teo.sprint.navogue.domain.user.repository.UserRepository;
 import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.net.URLEncoder;
 
 @Service
 @AllArgsConstructor
@@ -24,9 +26,11 @@ public class UserService {
 
     public String login(String code) throws Exception {
         String access_token = getAccessToken(code);
+        //String access_token = "vvp-Po2afSR_X3yBbbAM0pXvcdq1vAegarlzMcNyCj1zmwAAAYjvFLgW";
         String email = getUserInfo(access_token);
+        System.out.println("==========" + access_token + "   " + email);
 
-        if (userRepository.findByEmail(email) == null) {
+        if (userRepository.findByEmail(email).isEmpty()) {
             User user = new User(email);
             userRepository.save(user);
         }
@@ -37,48 +41,56 @@ public class UserService {
     }
     public String getAccessToken(String code) throws Exception {
         String access_Token = "";
-        String reqURL = "https://kauth.kakao.com/oauth/token";
-        String clientId = "ba988a6d89e2e66af10b99f9f7bdb97b";
-        String redirectUri = "http://localhost:5173/oauth";;
+        String url = "https://kauth.kakao.com/oauth/token";
+        String grantType = "authorization_code";
+        String clientId = URLEncoder.encode("b75280e464d73acb9af70f0fb5027d0c", "UTF-8");
+        String redirectUri = URLEncoder.encode("http://localhost:8082/auth/login", "UTF-8");
+        String code1 = URLEncoder.encode(code, "UTF-8");
 
+        String params = String.format("grant_type=%s&client_id=%s&redirect_uri=%s&code=%s",
+                grantType, clientId, redirectUri, code1);
 
-        URL url = new URL(reqURL);
-        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        URL obj = new URL(url);
+        HttpURLConnection connection = (HttpURLConnection) obj.openConnection();
 
-        conn.setRequestMethod("POST");
-        conn.setDoOutput(true);
+        connection.setRequestMethod("POST");
+        connection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
 
+        connection.setDoOutput(true);
+        DataOutputStream wr = new DataOutputStream(connection.getOutputStream());
+        wr.writeBytes(params);
+        wr.flush();
+        wr.close();
 
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(conn.getOutputStream()));
-        StringBuilder sb = new StringBuilder();
-        sb.append("grant_type=authorization_code");
+        int responseCode = connection.getResponseCode();
+        BufferedReader in = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+        String inputLine;
+        StringBuilder response = new StringBuilder();
 
-        sb.append("client_id=" + clientId);
-        sb.append("&redirect_uri=" + redirectUri);
-
-        sb.append("&code=" + code);
-        bw.write(sb.toString());
-        bw.flush();
-
-        BufferedReader br = new BufferedReader(new InputStreamReader(conn.getInputStream()));
-        String line = "";
-        String result = "";
-
-        while ((line = br.readLine()) != null) {
-            result += line;
+        while ((inputLine = in.readLine()) != null) {
+            response.append(inputLine);
         }
+        in.close();
 
-        JsonParser parser = new JsonParser();
-        JsonElement element = parser.parse(result);
+        System.out.println("Response Code: " + responseCode);
+        System.out.println("Response Body: " + response.toString());
+        String jsonResponse = response.toString();
 
-        access_Token = element.getAsJsonObject().get("access_token").getAsString();
-
-        br.close();
-        bw.close();
+        // TODO :: 파싱하기 {"access_token":"bqPA-y1fSjDQTmb2HBggSPp3YM1rdxGh7N7gczXNCj10mQAAAYjvGrOj","token_type":"bearer","refresh_token":"OSusG25A9bI2GRcUkj8EXkcm0_4h5AZcadfGk1qtCj10mQAAAYjvGrOh","expires_in":7199,"scope":"account_email","refresh_token_expires_in":5183999}
+        String accessTokenStart = "\"access_token\":\"";
+        int accessTokenStartIndex = jsonResponse.indexOf(accessTokenStart);
+        if (accessTokenStartIndex != -1) {
+            int accessTokenEndIndex = jsonResponse.indexOf("\"", accessTokenStartIndex + accessTokenStart.length());
+            if (accessTokenEndIndex != -1) {
+                String accessToken = jsonResponse.substring(accessTokenStartIndex + accessTokenStart.length(), accessTokenEndIndex);
+                access_Token = accessToken;
+            }
+        }
 
         log.info("access_token = ", access_Token);
         return access_Token;
     }
+
 
     public String getUserInfo(String access_Token) throws Exception {
         String reqURL = "https://kapi.kakao.com/v2/user/me";
